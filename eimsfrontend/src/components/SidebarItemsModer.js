1a40011d4bc1a328da4f4b84c1678baf6d5832db
DuongNH8837 <85751304+DuongNH8837@users.noreply.github.com>
2023-04-20 16:33:52 +0700

    Update SidebarItemsModer.js

--- eimsfrontend/src/components/SidebarItemsModer.js
+++ eimsfrontend/src/components/SidebarItemsModer.js
@@ -4,11 +4,6 @@ const SidebarItemsModer = [
         id:1,
         name: "Quản lý tài khoản",
         route: '/accountmanage'
-    },
-    {
-        id:2,
-        name: "Gói đăng ký",
-        route: '/subcribemanage'
     }
 ];
 
